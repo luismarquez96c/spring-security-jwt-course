039c95ddee0eadf6ce3e04be6af9eceb058ed5f9
PineberryCode <applepie090902@gmail.com>
2023-09-27 13:52:35 -0500

    Adding lombok

--- src/main/java/com/cursos/springsecuritycourse/entity/Product.java
+++ src/main/java/com/cursos/springsecuritycourse/entity/Product.java
@@ -7,10 +7,14 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
 public class Product {
 
     @Id
@@ -23,28 +27,5 @@ public class Product {
 
     @DecimalMin(value = "0.01")
     private BigDecimal price;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+    
 }
--- src/main/java/com/cursos/springsecuritycourse/util/Role.java
+++ src/main/java/com/cursos/springsecuritycourse/util/Role.java
@@ -3,22 +3,18 @@ package com.cursos.springsecuritycourse.util;
 import java.util.Arrays;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
 public enum Role {
 
     CUSTOMER(Arrays.asList(Permission.READ_ALL_PRODUCTS)),
     ADMINISTRATOR(Arrays.asList(Permission.SAVE_ONE_PRODUCT, Permission.READ_ALL_PRODUCTS));
 
+    @Getter
+    @Setter
     private List<Permission> permissions;
 
-    Role(List<Permission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public List<Permission> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(List<Permission> permissions) {
-        this.permissions = permissions;
-    }
 }
